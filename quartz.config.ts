19cc1c9923465137693b3f7016586ead52b05bab
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2024-07-07 20:50:09 +0400

    Quartz sync: Jul 7, 2024, 8:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: Чудесная Гостиная - Михаил Изюмов,
+    pageTitle: "Чудесная Гостиная - Михаил Изюмов",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
