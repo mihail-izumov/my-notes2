29f27887488544cb46fff264efb9d1abff4ea25a
Maxim Izyumov <maximizyumov@Mac-mini-Maxim.local>
2024-07-10 21:13:07 +0400

    Quartz sync: Jul 10, 2024, 9:13 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,18 +18,18 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [ 
-    Component.PageTitle(),
+    //Component.PageTitle(),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
-    //Component.PageTitle(),
+    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     
-    //Component.Search(),
-    //Component.Darkmode(),
+    Component.Search(),
+    Component.Darkmode(),
     //Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
