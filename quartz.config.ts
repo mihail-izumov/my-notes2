05e7de165f35cdbb062b3ed21982936ab0950d0d
Maxim Izyumov <maximizyumov@Mac-mini-Maxim.local>
2024-07-10 13:21:25 +0400

    Quartz sync: Jul 10, 2024, 1:21 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Чудесная Гостиная - Михаил Изюмов",
+    pageTitle: "Чудесная Гостиная Модуля Роста™",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
